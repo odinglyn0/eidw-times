a2c515dcf47d97a867f3ef4dd7efd824aa1d6507
[dyad] <git@dyad.sh>
2025-06-20 00:12:55 +0100

    [dyad] Removed the main heading from the Index page. - wrote 1 file(s) + extra files edited outside of Dyad

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,7 +5,7 @@ const Index = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-900 p-4">
       <div className="text-center mb-8">
-        <h1 className="text-4xl font-bold mb-2 text-gray-900 dark:text-gray-100">Dublin Airport Security Times</h1>
+        {/* Removed the h1 tag */}
         <img
           src={DublinAirportLogo}
           alt="Dublin Airport Logo"
