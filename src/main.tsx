64b9f1462b6f2bb7bf8caccf419cb12e217a97f6
Odin Glynn-Martin <odin.glynn@icloud.com>
2026-02-10 17:58:32 +0000

    chore(main): remove ASCII art banner from startup console output
    - Remove decorative ASCII art console.log statements from application startup
    - Simplify console output by keeping only essential initialization messages
    - Reduce unnecessary console noise during application initialization

--- src/main.tsx
+++ src/main.tsx
@@ -40,32 +40,5 @@ console.log(`%c-----------------------------------------------------------------
 ----------------------------------------------------------------------
 ----------------------------------------------------------------------`, "color: #4ade80; background: #000; font-size: 10px;");
 
-console.log(`%c
-                                                                                                                                                                                                                                                                                                                    
-                                                                                                                           KK        KK                           
-                                                          AA                                AA         AAA              KKKGGKKK  KKKGGKKK                        
-                        AAAA     AAAAA                    AAA                              AAAA  AAA   AAA             KFFFEFFFKKKGFFEFFFFK                       
-                        AAAAA   AAAAAA    AAAA       AAA  AAA    AAAA       AAA   AAA   AA  AA AAAAAAA AAA AAAA        KFFFFFFFFFFFFFFFFFFFK                      
-                        AAAAAA  AAAAAA  AAAAAAAA   AAAAAAAAAA  AAAAAAAA     AAA  AAAA  AAA AAAAAAAAAAA AAAAAAAAA       KFFFFUHFFFFFUFFFFEGK                       
-                        AAA AAAAAA AAA        AAA AAA    AAAA AAA    AAA     AAA AAAAA AAA AAAA  AAA   AAA   AAAA       KGFUFFFFFFFFFFFFFK                        
-                        AAA AAAAA  AAA  AAAAAAAAA AAA     AAA AAAAAAAAAA     AAAAAAAAAAAA  AAAA  AAA   AAA    AAA        KKFFFFFFFFFFFFKK                         
-                        AAA  AAA   AAA AAAA  AAAA AAAA   AAAA AAA   AAAA      AAAAAAAAAAA  AAAA  AAA   AAA    AAA         KKKFFFFFFFFKKK                          
-                        AAA   A    AAA  AAAAAAAAAA AAAAAAAAAA  AAAAAAAA       AAAAA AAAA   AAAA  AAAAA AAA    AAA           KKKFFFFKKK                            
-                                                                                                                              KKKKKK                              
-                                                                                                                                 K                                
-                                                                                                                                                                  
-                                                                                                                                                                  
-        AAAAAA                                                                          AAA  AAA                       AAA                          AAAAA         
-        AAAAA                                         AAAAAAAAAAAAA                     AAA  AAA                   AAA AAA                        AAAAAAAAA       
-      AAAAAAA  AAAAA  AAAAAA    AA AAAA  AAAAA        AA         AA    AAAAAAA    AAAAAAAAA  AAA  AAAAAAAA    AAAAAAAA AAA AAA    AAAAAAAAAAAAA   AAA   AAAA      
-      AAAAAAA AAAAAAAAAAAAAAAA  AAAAAAAAAAAAAAA       AA AAAAAAA AA   AAAAAAAAA  AAAAAAAAAA  AAA AAAAAAAAAA  AAA  AAA  AAA AAAA  AAAA AAAAAAAAAA AAAA   AAAA      
-        AAA   AAAA  AAA    AAA  AAA   AAA   AAA       AA AA  AAA AA  AAAA   AAAAAAAA    AAA  AAA AAAA   AAAA AAA  AAA  AAA   AAA AAA  AAAA   AAA AAAA   AAAA      
-        AAA   AAAA  AAA    AAA  AAA   AAA   AAA       AA AAAAAAAAAA  AAAA   AAAAAAAA    AAA  AAA AAAA   AAAA AAAAAAA   AAA   AAAAAA   AAA    AAA AAAA   AAAA      
-        AAA   AAAA  AAAAAAAAAA  AAA   AAA   AAA       AA   AA AAAAA   AAAAAAAAA  AAAAAAAAAA  AAA AAAA   AAAA AAAAAAAA  AAA    AAAA    AAA    AAA  AAAAAAAAA       
-        AAA   AAA     AAAAAA    AAA   AAA   AAA       AAAAAAAAAAAAA    AAAAAAA    AAAAA AAA  AAA  AAA   AAAA AAAAAAAAA AAA     AAA    AAA    AAA    AAAAAA        
-                                                                                                             AAAAAAAAA       AAAAA                                
-                                                                                                               AAAA                                               
-
-`, "color: #4ade80; background: #000; font-size: 10px;");
 
 createRoot(document.getElementById("root")!).render(<App />);
